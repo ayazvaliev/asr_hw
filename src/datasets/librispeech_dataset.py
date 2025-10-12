dbb9ed72864f1d88c11b89396ed87c82dc61166c
Ayaz <valiewayaz2003@gmail.com>
2025-10-12 11:00:32 +0300

    fix librispeech dataset index dir

--- src/datasets/librispeech_dataset.py
+++ src/datasets/librispeech_dataset.py
@@ -22,12 +22,16 @@ URL_LINKS = {
 
 
 class LibrispeechDataset(BaseDataset):
-    def __init__(self, part, data_dir=None, *args, **kwargs):
+    def __init__(self, part, data_dir=None, index_dir=None, *args, **kwargs):
         assert part in URL_LINKS or part in {"train_all", "train_clean"}
         if data_dir is None:
             data_dir = ROOT_PATH / "data" / "datasets" / "librispeech"
             data_dir.mkdir(exist_ok=True, parents=True)
         self._data_dir = Path(data_dir)
+        if index_dir is None:
+            index_dir = self._data_dir
+        self._index_dir = Path(index_dir)
+
         if part == "train_all":
             index = [self._get_or_load_index(part) for part in URL_LINKS if "train" in part]
         elif part == "train_clean":
@@ -49,7 +53,7 @@ class LibrispeechDataset(BaseDataset):
         shutil.rmtree(str(self._data_dir / "LibriSpeech"))
 
     def _get_or_load_index(self, part):
-        index_path = self._data_dir / f"{part}_index.json"
+        index_path = self._index_dir / f"{part}_index.json"
         if index_path.exists():
             with index_path.open() as f:
                 index = json.load(f)
