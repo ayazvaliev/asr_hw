218c13d70f9caf4506e6e40c11ed2cd2912918c6
Ayaz <valiewayaz2003@gmail.com>
2025-10-12 11:25:39 +0300

    fix base dataset

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -28,7 +28,7 @@ class BaseDataset(Dataset):
         limit=None,
         max_audio_length=None,
         max_text_length=None,
-        shuffle_index=False,
+        shuffle_index=True,
         instance_transforms=None,
     ):
         """
