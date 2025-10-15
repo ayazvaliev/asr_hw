468f2977117ca8796988a3a0542f754869696c50
Ayaz <valiewayaz2003@gmail.com>
2025-10-15 14:13:39 +0300

    add debug info

--- src/loss/ctc_loss.py
+++ src/loss/ctc_loss.py
@@ -10,6 +10,8 @@ class CTCLossWrapper(CTCLoss):
     def forward(
         self, log_probs, log_probs_length, text_encoded, text_encoded_length, **batch
     ) -> Tensor:
+        if torch.any(log_probs_length < text_encoded_length):
+            print(f'log_probs_length < text_encoded_length: {log_probs_length}, {text_encoded_length}')
         loss = super().forward(
             log_probs=log_probs,
             targets=text_encoded,
