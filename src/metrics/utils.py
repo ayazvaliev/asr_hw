4837dd944458ace9a3bc8d40d5b9d53ea3deffd3
Ayaz <valiewayaz2003@gmail.com>
2025-10-15 18:13:43 +0300

    fix cer wer

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -5,13 +5,13 @@
 import editdistance
 
 
-def calc_cer(target_text: str, predicted_text: str) -> float | None:
+def calc_wer(target_text: str, predicted_text: str) -> float | None:
     if len(target_text.split()) == 0:
         return None
     return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text.split())
 
 
-def calc_wer(target_text: str, predicted_text: str) -> float:
+def calc_cer(target_text: str, predicted_text: str) -> float:
     if len(target_text) == 0:
         return None
     return editdistance.eval(target_text, predicted_text) / len(target_text)
